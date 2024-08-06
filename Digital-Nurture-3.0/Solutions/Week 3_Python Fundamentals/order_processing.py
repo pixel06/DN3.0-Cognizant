54dd0b1c4d523f2928387666c97e335a346b1fe0
G V N Sridhar <95140879+pixel06@users.noreply.github.com>
2024-08-06 15:52:40 +0530

    Delete Digital-Nurture-3.0 directory

--- Digital-Nurture-3.0/Solutions/Week 3_Python Fundamentals/order_processing.py
+++
@@ -1,8 +0,0 @@
-def apply_discount(order_amount):
-    if(order_amount>=100):
-        discount = 0.1 * order_amount
-        return order_amount - discount
-    else:
-        return order_amount
-order_amount=int(input("Enter the order value:"))
-print("The amount after discount is:",apply_discount(order_amount))
--- Digital-Nurture-3.0/Solutions/Week 3_Python Fundamentals/payroll_calculator.py
+++
@@ -1,5 +0,0 @@
-def calculate_pay(hours,rate):
-    return hours*rate
-hours=int(input("Enter the number of hours worked:"))
-rate=int(input("Enter the rate per hour: "))
-print("Total Payroll for the day is:",calculate_pay(hours,rate)) 
